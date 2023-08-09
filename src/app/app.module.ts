156a4dfee9f0c99bec9b38780d1a51027798e3cb
carollyb <carolly.barbosa@gmail.com>
2023-08-08 21:49:50 -0300

    style: adds icons, improves style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,22 @@ import { AppComponent } from './app.component';
 import { CardComponent } from './components/card/card.component';
 import { HttpClientModule } from '@angular/common/http';
 
+import { NgIconsModule } from '@ng-icons/core';
+import {
+  heroUsers,
+  heroCheck,
+  heroXMark,
+  heroClock,
+} from '@ng-icons/heroicons/outline';
+
 @NgModule({
   declarations: [AppComponent, CardComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    NgIconsModule.withIcons({ heroUsers, heroCheck, heroXMark, heroClock }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
